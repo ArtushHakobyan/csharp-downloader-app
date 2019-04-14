b7d4d7a939d13811f1de93aa0e0a4a8b1c9ac7c6
Artush Hakobyan <artush.hakobyan3@gmail.com>
2019-04-14 02:18:17 -0700

    add Extension methods for Uri and WebClient classes

---
+++ DownloaderApp/UriExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DownloaderApp
+{
+    public static class UriExtensions
+    {
+        public static string GetFileName(this Uri uri)
+        {
+            string str = uri.OriginalString;
+            string[] stringParts = str.Split('/');
+            string fileName = string.Empty;
+
+            if (stringParts.Length > 0)
+            {
+                fileName = stringParts[stringParts.Length - 1];
+            }
+            else
+            {
+                fileName = str;
+            }
+
+            return fileName;
+        }
+    }
+}
---
+++ DownloaderApp/WebClientExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace DownloaderApp
+{
+    public static class WebClientExtensions
+    {
+        public static Download GetDownload(this WebClient client)
+        {
+            foreach (var download in MainWindow.downloads)
+            {
+                if(download.Client == client)
+                {
+                    return download;
+                }
+            }
+
+            return null;
+        }
+    }
+}
